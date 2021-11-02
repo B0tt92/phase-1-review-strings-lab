b87070bf348c0364f9d9da5c293f39c0f66a1546
+branden <+branden_ott@outlook.com+>
2021-11-02 13:39:39 -0400

    commit tree ee343cbe7ae791d9a0028813a1f6c31b29ca90c5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'Bott bott';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const welcomeMessage = `Welcome to Flatbook,   ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(5);
+let shortGreeting = `Welcome, ${firstInitial}${restOfName}!`;
+console.log(shortGreeting)
